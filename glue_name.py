0d54891b17ee51652e5cc3c545f7cdae66e2e63f
studentDRG <info@oscqr.com>
2025-02-19 13:21:21 -0500

    Update glue_name.py

--- glue_name.py
+++ glue_name.py
@@ -19,7 +19,7 @@
     
 first_name = input('Enter first name: ')
 last_name = input('Enter last name: ')
-print(f'Hi, {first_name}{last_name}')    
+print(f'Hi, {first_name} {last_name}')    
     
     
     
